fc433ee0e82f8e7339aabcdfe9f0896f8f6847ce
Ziehe Enrico <E.Ziehe@strube.net>
2019-08-27 20:42:06 +0200

    Plugin exportiert

--- src/Plugins/Strube.Export/Providers/OrderFullExportProvider.cs
+++ src/Plugins/Strube.Export/Providers/OrderFullExportProvider.cs
@@ -14,6 +14,8 @@ using SmartStore.Services.Catalog;
 using SmartStore.Services.DataExchange.Export;
 using SmartStore.Services.Directory;
 using SmartStore.Services.Localization;
+using System.IO;
+using SmartStore.Core.Domain.Orders;
 
 namespace Strube.Export.Providers
 {
@@ -36,6 +38,11 @@ namespace Strube.Export.Providers
             get { return ExportEntityType.Order; }
         }
 
+        public static string SystemName
+        {
+            get { return "Strube.OrdersExportCSV"; }
+        }
+
         public override string FileExtension
         {
             get { return "txt"; }
@@ -43,7 +50,75 @@ namespace Strube.Export.Providers
 
         protected override void Export(ExportExecuteContext context)
         {
-            throw new NotImplementedException();
+            dynamic currency = context.Currency;
+            string _FormatString = "{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11};{12};{13}";
+
+            //Create Streamwriter
+            StreamWriter _sw = new StreamWriter(context.DataStream);
+            //Add Header Line
+            _sw.WriteLine(String.Format(_FormatString, 
+                "ID", 
+                "OrderId", 
+                "Comment",
+                "Company",
+                "Name",
+                "Surname",
+                "Address1",
+                "Address2",
+                "Zip-Code",
+                "City",
+                "Country",
+                "ItemId",
+                "Description",
+                "Count"));
+            // export the lines
+            while (context.Abort==DataExchangeAbortion.None && context.DataSegmenter.ReadNextSegment())
+            {
+                var segment = context.DataSegmenter.CurrentSegment;
+                foreach (dynamic order in segment)
+                {
+                    Order orderEntity = order.Entity;
+                    List<OrderItem> orderItem = orderEntity.OrderItems.ToList();
+
+                    if (context.Abort!= DataExchangeAbortion.None)
+                    {
+                        break;
+                    }
+
+                    try
+                    {
+                        foreach (OrderItem item in orderItem)
+                        {
+                            Product itemProduct = item.Product;
+                            _sw.WriteLine(String.Format(_FormatString,
+                                orderEntity.OrderGuid,
+                                orderEntity.GetOrderNumber(),
+                                orderEntity.CustomerOrderComment,
+                                orderEntity.ShippingAddress.Company,
+                                orderEntity.ShippingAddress.LastName,
+                                orderEntity.ShippingAddress.FirstName,
+                                orderEntity.ShippingAddress.Address1,
+                                orderEntity.ShippingAddress.Address2,
+                                orderEntity.ShippingAddress.ZipPostalCode,
+                                orderEntity.ShippingAddress.City,
+                                orderEntity.ShippingAddress.Country.Name,
+                                itemProduct.Sku,
+                                itemProduct.Name,
+                                item.Quantity
+                                )) ;
+
+                            ++context.RecordsSucceeded;
+
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        context.RecordException(ex, orderEntity.Id);
+                    }
+                }
+            }
+            _sw.Flush();
+            //throw new NotImplementedException();
         }
 
         //public override ExportConfigurationInfo ConfigurationInfo
